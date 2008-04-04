5a62a60144f9acd0e28059a6f1a9d7ffbd80643b
cjolley <cjolley@394f415c-e224-0410-a11c-cb241aa5d150>
2008-04-04 23:46:54 +0000

    - Added utilities methods for working with a Range - Improved performance of String.fmt() in FireFox. - First cut at new incremental-rendering CollectionView.  This new view requires you to use absolutely positioned views.  Performance is outstanding.
    git-svn-id: https://sproutcore.googlecode.com/svn/trunk/samples@10 394f415c-e224-0410-a11c-cb241aa5d150

--- clients/sample_controls/main.js
+++ clients/sample_controls/main.js
@@ -15,11 +15,18 @@ function main() {
   // The default code here will load the fixtures you have defined.
   // Comment out the preload line and add something to refresh from the server
   // when you are ready to pull data from your server.
+  SC.Benchmark.start('load fixtures') ;
   SampleControls.server.preload(SampleControls.FIXTURES) ;
+  SC.Benchmark.start('end fixtures') ;
 
   // Set content controller for display
+  SC.Benchmark.start('SampleControls.contentController.content') ;
   SampleControls.contentController.set('content', SampleControls.Contact.findAll());
+  SC.Benchmark.end('SampleControls.contentController.content') ;
+
+  SC.Benchmark.start('SC.page.controlTabs.nowShowing = collections') ;
   SC.page.get('controlTabs').set('nowShowing', 'collections') ;
+  SC.Benchmark.end('SC.page.controlTabs.nowShowing = collections') ;
   
 } ;
 
