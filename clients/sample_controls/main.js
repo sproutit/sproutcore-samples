ca165bbb38931c3a45c808575a84ca2983c438f4
cjolley <cjolley@394f415c-e224-0410-a11c-cb241aa5d150>
2008-04-28 03:13:30 +0000

    Housekeeping fixes.
    * Initial changes required to eventually implement improved observer 
    notification system.
    
    * Improved documentation for a variety of classes
    
    * [FIX] Only fixtures ending in ".js" will now be loaded.  This allows you to include fixtures named .json or whatever and actually load them via XHR.
    
    * [FIX] The build system now ignored any rhtml files that does not appear in
    an lproj directory since this is where templates are stored.
    
    * Renamed some panes to include demos for panes.
    
    
    
    git-svn-id: https://sproutcore.googlecode.com/svn/trunk/samples@74 394f415c-e224-0410-a11c-cb241aa5d150

--- clients/sample_controls/main.js
+++ clients/sample_controls/main.js
@@ -40,10 +40,9 @@ function main() {
   SC.Benchmark.end('SampleControls.sourceListController.content') ;
 
   SC.Benchmark.start('SC.page.controlTabs.nowShowing = collections2') ;
-  SC.page.get('controlTabs').set('nowShowing', 'collections2') ;
+  SC.page.get('controlTabs').set('nowShowing', 'panes') ;
   SC.Benchmark.end('SC.page.controlTabs.nowShowing = collections2') ;
   
-  SC.page.get('footer') ;
-  
+  SC.page.get('pickerPane') ;
 } ;
 
