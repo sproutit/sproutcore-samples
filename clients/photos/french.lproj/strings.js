fc9f3b8b08ec5efc7011944df850fd4bbc880afb
cjolley <cjolley@394f415c-e224-0410-a11c-cb241aa5d150>
2008-05-16 20:18:16 +0000

    * [IE] innerFrame now returns the correct value for elements without hasLayout.
    * [IE] get styleWidth & styleHeight on a view will now return the same value
    on all platforms.  The IE version would previously include padding and borders
    even though it should include only the core width.
    
    * [FIX] Unit tests could fail to execute in IE if they ended in a comment, fixed unittest.js to avoid this problem.
    
    * [FIX] TextFieldView could blank out content when you tabbed out of in.
    
    == SproutCore 0.9.5
    
    * Build Tools will now remove any loc strings from the strings.js file beginning with "@@" for the key name when building the JS file to send to the client.  This allows you to include strings in the strings.js file that you only want to use for server-side localization.
    
    * Build Tools now support the "loc()" ruby helper, which will include a server-side loc string pulled from the strings.js file for the current bundle.
    
    * Collection View now supports selectOnMouseDown which can be turned off to provide better drag and drop behavior on SourceList.  SC.SourceListView now turns this off by default.
    
    
    
    git-svn-id: https://sproutcore.googlecode.com/svn/trunk/samples@99 394f415c-e224-0410-a11c-cb241aa5d150

--- clients/photos/french.lproj/strings.js
+++ clients/photos/french.lproj/strings.js
@@ -9,9 +9,7 @@
 // localized string added to this file!
 //
 Object.extend(String.English,{
-  "Another test": "Like This",
-  '@@_Test Loc String "With Quotes"': 'English Test Loc String \"With Quotes\"',
-  "Here is another line": "That I can edit"
+  '@@_Test Loc String "With Quotes"': "French Test Loc String \"With Quotes\""
   // "_String Key": "Localized String"
 }) ;
 
