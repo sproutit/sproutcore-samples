7e04fc789324fc9d2f545dc83dc23a0ad748ab37
Glenn Rempe <glenn@rempe.us>
2008-06-25 16:15:54 -0700

    Twitter fixtures cleanup.

--- clients/twitter/fixtures/status.js
+++ clients/twitter/fixtures/status.js
@@ -11,11 +11,11 @@ Twitter.FIXTURES = Twitter.FIXTURES.concat([
   // name of your contact.  See the example below.
 
   {
-      "in_reply_to_status_id": null,
-      "favorited": false,
-      "source": "<a href=\"http://iconfactory.com/software/twitterrific\">twitterrific</a>",
-      "id":840664999,
-      guid:840664999,
+    "in_reply_to_status_id": null,
+    "favorited": false,
+    "source": "<a href=\"http://iconfactory.com/software/twitterrific\">twitterrific</a>",
+    "id":840664999,
+    guid:840664999,
     "user": 3462491,
     "in_reply_to_user_id":null,
     "truncated":false,
@@ -23,35 +23,5 @@ Twitter.FIXTURES = Twitter.FIXTURES.concat([
     "created_at":"Sun Jun 22 03:10:40 +0000 2008",
     type: "Status"
   }
-
-  // { guid: 1,
-  //   type: 'Contact',
-  //   firstName: "Michael",
-  //   lastName: "Scott"
-  // },
-  //
-  // { guid: 2,
-  //   type: 'Contact',
-  //   firstName: "Dwight",
-  //   lastName: "Schrute"
-  // },
-  //
-  // { guid: 3,
-  //   type: 'Contact',
-  //   firstName: "Jim",
-  //   lastName: "Halpert"
-  // },
-  //
-  // { guid: 4,
-  //   type: 'Contact',
-  //   firstName: "Pam",
-  //   lastName: "Beesly"
-  // },
-  //
-  // { guid: 5,
-  //   type: 'Contact',
-  //   firstName: "Ryan",
-  //   lastName: "Howard"
-  // }
 
 ]);
