28251eb18a0b4f2f0b16026170b76f93146dff3a
Charles Jolley <charles@sproutit.com>
2008-06-27 20:16:25 -0700

    Update to the latest sproutcore

---
+++ frameworks/sproutcore/controllers/array.js
@@ -0,0 +1,298 @@
+// ========================================================================
+// SproutCore
+// copyright 2006-2008 Sprout Systems, Inc.
+// ========================================================================
+
+require('controllers/controller') ;
+require('mixins/array') ;
+require('mixins/selection_support') ;
+require('foundation/binding') ;
+
+/** @class
+
+An Array Controller provides a way to project the contents of an array
+out to a view.  You can use this object any place you might use an
+array.  Changes to the array will not propogate to the content array
+until you call commitChanges().
+
+@extends SC.Controller
+@extends SC.Array
+@extends SC.SelectionSupport
+@since SproutCore 1.0
+
+*/
+SC.ArrayController = SC.Controller.extend(SC.Array, SC.SelectionSupport,
+/** @scope SC.ArrayController.prototype */
+{
+  /**
+    If YES the will return controllers for content objects.
+    
+    If you want to use an array controller to edit an array contents directly
+    but you do not want to wrap the values of the array in controller objects
+    then you should set this property to NO.
+    
+    @field
+    @type {Boolean}
+  */
+  useControllersForContent: NO,
+  
+  /**
+    Provides compatibility with CollectionControllers.
+    @field
+    @type {SC.ArrayController}
+  */
+  arrangedObjects: function() { return this; }.property('content'),
+
+  /**
+    The array content that is being managed by the controller.
+    @field
+    @type {Array}
+  */
+  content: null,
+  contentBindingDefault: SC.Binding.Multiple,
+  
+  /**
+    Set to true if you want objects removed from the array to also be
+    deleted.  This is a convenient way to manage lists of items owned
+    by a parent record object.
+    
+    @field
+    @type {Boolean}
+  */
+  destroyOnRemoval: NO,
+    
+  /**
+    Watches changes to the content property updates the contentClone.
+    @private
+    @observes content
+  */
+  _contentObserver: function() {
+    var content = this.get('content') ;
+    if (SC.isEqual(content, this._content)) return ; // nothing to do
+
+    if (!this._boundContentPropertyObserver) {
+      this._boundContentPropertyObserver = this._contentPropertyObserver.bind(this) ;
+    }
+    var func = this._boundContentPropertyObserver ;
+
+    // remove old observer, add new observer, and trigger content property change
+    if (this._content && this._content.removeObserver) this._content.removeObserver('[]', func) ;
+    if (content && content.addObserver) content.addObserver('[]', func) ;
+    this._content = content; //cache
+    this._contentPropertyRevision = null ;
+    
+    var rev = (content) ? content.propertyRevision : -1 ;
+    this._contentPropertyObserver(this, '[]', content, rev) ; 
+  }.observes('content'),
+
+  _contentPropertyObserver: function(target, key, value, rev) {  
+      
+    if (!this._updatingContent && (!rev || (rev != this._contentPropertyRevision))) {
+      this._contentPropertyRevision = rev ;
+
+      this._updatingContent = true ;
+
+      this.beginPropertyChanges();
+      this.contentCloneReset();
+      this.arrayContentDidChange() ;
+      this.notifyPropertyChange('length') ;
+      this.updateSelectionAfterContentChange();
+      this.endPropertyChanges() ;
+
+      this._updatingContent = false ;
+
+    }
+  },
+  
+  /**
+    The array content that (when committed) will be merged back into the 
+    content property. All array methods will take place on this object.
+
+    @field
+    @type {SC.Array}
+  */
+  contentClone: null,
+
+  /**
+    Clones the content property into the contentClone property.
+    @private
+  */
+  contentCloneReset: function() {
+    this._changelog = [];
+    this.set('contentClone', null);
+  },
+
+  /**
+   SC.Array interface implimentation.
+   
+   @param idx {Number} Starting index in the array to replace.  If idx >= 
+     length, then append to the end of the array.
+   
+   @param amt {Number} Number of elements that should be removed from the 
+     array, starting at *idx*.
+   
+   @param objects {Array} An array of zero or more objects that should be 
+     inserted into the array at *idx* 
+  */
+  replace: function(idx, amt, objects) {
+
+    var content = this.get('content') ;
+
+    // in case the passed objects are controllers, convert to source objects.
+    var copyIdx = objects.length ;
+    var sourceObjects = [] ;
+    while(--copyIdx >= 0) sourceObjects[copyIdx] = this._sourceObjectFor(objects[copyIdx]) ;
+    
+    // create clone of content array if needed
+    var contentClone = this.get('contentClone') ;
+    if (!contentClone) {
+      this.set('contentClone', contentClone = content.clone()) ;
+    }
+
+    // now, record the removed objects.  This may be used later.
+    if (this.get('destroyOnRemoval')) {
+      if (!this._deletions) this._deletions = [] ;
+      for (var i=0; i < amt; i++) {
+        this._deletions.push(content.objectAt(idx + i));
+      }
+    }
+
+    // and record additions
+    if (!this._changelog) this._changelog = []; 
+    this._changelog.push({ idx: idx, amt: amt, objects: sourceObjects });
+    
+    // then actually perform the edit on the contentClone
+    contentClone.replace(idx, amt, sourceObjects);
+    
+    this.editorDidChange() ;
+    this.arrayContentDidChange();
+    this.updateSelectionAfterContentChange();
+    
+    return this;
+  },
+  /**
+  * SC.Array interface implimentation.
+  * 
+  * @param {Number} idx
+  *   The index of the item to return.  If idx exceeds the current length, 
+  *   return null.
+  */
+  objectAt: function(idx) {
+    var obj = this._getSourceContent() ;
+    obj = (obj && obj.objectAt) ? obj.objectAt(idx) : null;
+    return this._objectControllerFor(obj) ;
+  },
+  /**
+  * SC.Array interface implimentation.
+  * @field
+  * @type {integer}
+  */
+  length: function( key, value ) {
+    var ret = this._getSourceContent() ;
+    return (ret && ret.get) ? (ret.get('length') || 0) : 0 ;
+  }.property(),
+
+  /**
+    Returns the index in the array of the specified object.
+    
+    This can handle both controller wrapper objects and source content objects.
+  */
+  indexOf: function( obj ) {
+    return this._getSourceContent().indexOf(this._sourceObjectFor(obj)) ;
+  },
+  
+  _getSourceContent: function() {
+    return this.get('contentClone') || this.get('content') || [];
+  },
+  
+  /** 
+  * @private
+  */
+  performCommitChanges: function()
+  {
+    var content = this.get('content');
+    var ret     = true;
+    
+    // cannot commit changes to null content.  Return an error.
+    if (!content) {
+      return $error("No Content");
+    }
+    
+    if (content.beginPropertyChanges) content.beginPropertyChanges();
+
+
+    // apply all the changes made to the clone
+    var changelog = this._changelog || [] ;
+    for(var idx=0;idx<changelog.length;idx++) {
+      var change = changelog[idx];
+      content.replace(change.idx, change.amt, change.objects) ;
+    }
+    
+    // done, flush the changelog
+    this._changelog = [] ;
+
+    // finally, destroy any removed objects if necessary.  Make 
+    // sure the objects have not been re-added before doing this.
+    if (this.get('destroyOnRemoval') && this._deletions && this._deletions.length>0) {
+      var idx = this._deletions.length;
+      while(--idx >= 0) {
+        var obj = this._deletions[idx] ;
+        if (obj && obj.destroy && (content.indexOf(obj) < 0)) {
+          obj.destroy() ; 
+        }
+      }
+      this._deletions = [] ; // clear array
+    }
+    
+     // finish commiting changes.
+    if (content.endPropertyChanges) content.endPropertyChanges();
+    if (content.commitChanges) ret = content.commitChanges();
+    
+    if ($ok(ret)) {
+      this.contentCloneReset();
+      this.editorDidClearChanges();
+    }
+    
+    return ret;
+  },
+  /** 
+  * @private
+  */
+  performDiscardChanges: function()
+  {
+    this.contentCloneReset();
+    this.editorDidClearChanges();
+    return true;
+  },
+  
+  /** @private
+    Returns the object controller for a source value.
+  */
+  _objectControllerFor: function(obj) {
+    if (!this.useControllersForContent) return obj;
+    
+    var controllers = this._objControllers = this._objControllers || {} ;
+    var guid = SC.getGUID(obj) ;
+    var ret = controllers[guid] ;
+    if (!ret) {
+      ret = controllers[guid] = this.controllerForValue(obj) ;
+      if (ret) ret.__isArrayController = true ;
+    }
+    return ret ;
+  },
+  
+  /** @private
+    Returns the source object for the passed value.  If the passed value is a 
+    controller, this will map back to the sourceo object.  Otherwise the object itself
+    will be returned.
+  */
+  _sourceObjectFor: function(obj) {
+    return (obj && obj.kindOf && obj.kindOf(SC.Controller)) ? obj.get('content') : obj ;
+  },
+  
+  init: function() {
+    arguments.callee.base.apply(this, arguments) ;
+    if (this.get('content')) this._contentObserver() ;
+  }
+
+});
---
+++ frameworks/sproutcore/foundation/string.js
@@ -0,0 +1,214 @@
+// ==========================================================================
+// SproutCore -- JavaScript Application Framework
+// copyright 2006-2008, Sprout Systems, Inc. and contributors.
+// ==========================================================================
+
+// These are basic enhancements to the string class used throughout 
+// SproutCore.
+// capitalize a string.
+Object.extend(String.prototype,{
+
+  // Capitalize a string.
+  //
+  // mode: optional.  'each' - capitalize each word. the default.
+  //                  'first' - capitalize the first word only.
+  
+  capitalize: function(mode) {
+    var words = (mode == 'first') ? this : this.split(' ') ;
+    words = words.map(function(word) {
+      if (word.length == 0) return word ;
+      return word.charAt(0).toUpperCase() + word.substring(1) ;
+    }) ;
+    return words.join(' ') ;
+  },
+
+  // Interpolate string. looks for %@ or %@1; to control the order of params.
+  format: function() {
+    var args = $A(arguments) ;
+    
+    // first, replace any ORDERED replacements.
+    var str = this.gsub(/%@([0-9]+)/, function(m) {
+      return (args[parseInt(m[1],0)-1] || '').toString(); 
+    }) ;
+
+    // now, replace any remaining %@ items.  Use this indexOf() method b/c
+    // it is faster than split().
+    var ret = [] ;
+    var idx = -1 ;
+    var loc = 0 ;
+    while((idx = str.indexOf("%@",loc)) >= 0) {
+     // slice off initial part of string and push into ret. update loc.
+     ret.push(str.slice(loc,idx)) ;
+     loc = idx + 2 ; // 2 to skip '%@'.
+     
+     // add in replacement.
+     var value = args.shift() ;
+     if (value && value.toString) value = value.toString() ;
+     ret.push(value) ;
+    }
+    
+    // include any remaining bits of the string.
+    if (loc < str.length) {
+      ret.push(str.slice(loc,str.length)) ;
+    }
+    
+    // join return value.
+    return (ret.length > 1) ? ret.join('') : ret[0] ;
+  },
+
+  // localize a string.  Also interpolates any items you pass just like
+  // format().
+  loc: function() {
+    // NB: This could be implemented as a wrapper to locWithDefault() but
+    // it would add some overhead to deal with the arguments and adds stack
+    // frames, so we are keeping the implementation separate.
+    
+    var kit = String[String.currentLanguage()];
+    var str = kit[this] ;
+    if (!str) str = String.English[this] || this ;
+    return str.format.apply(str,arguments) ;
+  },
+  
+  // this works just like loc except it will return the first argument
+  // as a default if the matching value is not found.
+  locWithDefault: function(def) {
+    var kit = String[String.currentLanguage()];
+    var str = kit[this] ;
+    if (!str) str = String.English[this] || def ;
+    var args = $A(arguments) ;
+    args.shift() ; // escape def.
+    return str.format.apply(str, args) ;
+  },
+  
+  classify: function() {
+    return this.camelize().capitalize() ;
+  },
+  
+  decamelize: function() { 
+    return this.replace(/([a-z])([A-Z])/g,'$1_$2').toLowerCase();
+  },
+
+  dasherize: function() {
+    return this.decamelize().replace(/[ _]/g,'-') ;  
+  },
+  
+  humanize: function() {
+    return this.decamelize().replace(/[-_]/g,' ') ;
+  },
+  
+  toHref: function() {
+    if (this.match(/.+@.+\...+/)) {
+      return 'mailto:' + this;
+    } else if (this.indexOf('http://') != 0 && this.indexOf('https://') !=0 && this.match(/[^.]+\.[^.]+/)) {
+      return 'http://' + this;
+    } else {
+      return this;
+    }
+  },
+  
+  trim: function ()
+  {
+    return this.replace(/^\s+|\s+$/g,"");
+  },
+  
+  strip: function()
+  {
+    return this.trim();
+  }
+  
+}) ;
+
+// Shorter alias of the format function.
+String.prototype.fmt = String.prototype.format ;
+
+// Add strings for various languages to this collection.  String.loc()
+// method will try to localize the string passed using the current language.
+// if the language is not available, it will use English.
+Object.extend(String,
+/** @scope String @static */ {
+
+  /**
+    The current browser language as a two letter code.
+  */
+  browserLanguage: ((navigator.language || navigator.browserLanguage).split('-', 1)[0]),
+  
+  /**
+    If YES, localization will favor the detected language instead of the
+    preferred one.
+  */
+  useAutodetectedLanguage: NO,
+  
+  /**
+    This property is set by the build tools to the current build language.
+  */
+  preferredLanguage: null,
+  
+  /**
+    Returns the hash key to use for loc strings.  The default implementation
+    will autodetect the browser language and look for a loc string to 
+    match.  If it can't find one then it will introspect to find loc strings
+    that are defined and use those instead.
+  */
+  currentLanguage: function () {
+    
+    var ret = (this.useAutodetectedLanguage) ? (this.browserLanguage || this.preferredLanguage || 'en') : (this.preferredLanguage || this.browserLanguage || 'en') ;
+
+    // then try a couple of normalized forms...
+    if (!this[ret]) ret = this.normalizedLanguage(ret);
+    return ret ;
+  },
+  
+  /**
+    Returns a normalized language string for the two letter country code.
+  */
+  normalizedLanguage: function(ret) {
+    switch(ret) {
+      case 'fr':
+        ret = 'French'; 
+        break ;
+      case 'de':
+        ret = 'German'; 
+        break ;
+      case 'ja':
+      case 'jp':
+        ret = 'Japanese'; 
+        break ;
+      case 'en':
+        ret = 'English' ;
+        break ;
+      
+      case 'es':
+        ret = 'Spanish' ;
+        break;
+        
+      default:
+        break ;
+    }
+    return ret;
+  },
+  
+  /**
+    Adds loc strings for the named language.  This method takes care of 
+    creating the localized string hash if it does not already exist.
+    The language can be one of the following or any two-letter country code.
+    
+    English, French, German, Japanese, Spanish
+    
+    @param language {String} the language code
+    @param strings {Hash} hash of loc strings.
+    @returns {this}
+  */
+  addStringsFor: function(language, strings) {    
+    // convert language to a normalized name...
+    language = String.normalizedLanguage(language) ;
+    if (!String[language]) String[language] = {} ;
+    Object.extend(String[language], strings || {}); 
+    return this;
+  }
+
+});
+
+String.English  = String.English  || {};
+String.French   = String.French   || {};
+String.German   = String.German   || {};
+String.Japanese = String.Japanese || {};
