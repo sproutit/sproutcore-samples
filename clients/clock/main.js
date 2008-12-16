75156c6845364e4c5ded709a1abb388f5dbfacc4
Charles Jolley <charles@sproutit.com>
2008-12-16 11:07:25 -0800

    Add clock demo

---
+++ clients/clock/main.js
@@ -0,0 +1,67 @@
+// ==========================================================================
+// Simple Clock
+// ==========================================================================
+
+/* @static
+  This demonstrates how to build a very simple application using the 
+  new SproutCore.  All applications should have some views, a controller
+  and a main function.
+  
+  Place your applicaiton into a namespace.
+  
+  @namespace Clock
+*/
+var Clock = SC.namespace('Clock', function(Clock) {
+
+  // define the controller
+  Clock.clockController = SC.Object.create({
+    
+    showGreeting: NO,
+    
+    value: function() {
+      return this.get(this.get('showGreeting') ? 'greeting' : 'now') ;
+    }.property('showGreeting', 'greeting', 'now').cacheable(),
+    
+    greeting: 'Hello World',
+    
+    now: '--',
+    
+    // updates now to reflect the clock
+    tick: function() {
+      this.set('now', new Date().format('hh:mm:ss'));
+      this.invokeLater(this.tick, 1000);
+    }
+  });
+
+  // define the views
+  Clock.mainPage = SC.Page.design({
+
+    // create the wrapper mainPane to handle display
+    mainPane: SC.MainPane.design()
+      .layout({ centerX: 0, centerY: 0, width: 320, height: 120 })
+      .styleClass('clock')
+      
+      // add labelView child...
+      .childView(SC.LabelView.design()
+        .layout({ left: 10, right: 10, centerY: 0, height: 48 })
+        .tagName('h1')
+        .bind('value', 'Clock.clockController.value'))
+        
+      .childView(SC.CheckboxView.design()
+        .layout({ centerX: 0, width: 130, bottom: 10, height: 18 })
+        .prop('title', 'Show Greeting')
+        .bind('value', 'Clock.clockController.showGreeting'))
+
+  }) ;
+
+  Clock.main = function() {
+    this.getPath('mainPage.mainPane').append();
+    this.clockController.tick(); // start ticking
+    
+    this.clockController.addProbe('showGreeting');
+  } ;
+});
+
+function main() { return Clock.main(); }
+
+
--- clients/view_builder/english.lproj/main_page.js
+++ clients/view_builder/english.lproj/main_page.js
@@ -10,35 +10,29 @@ ViewBuilder.mainPage = SC.Page.create({
   
   mainPane: SC.MainPane.design({
     childViews: [
-      SC.View.design({
-        childViews: [
-          SC.View.design({
-          })
-        ]
-      })
-      // SC.ButtonView.design({
-      //   title: "OK",
-      //   layout: { bottom: 20, right: 20, width: 80, height: 21 }
-      // }),
-      // 
-      // SC.ButtonView.design({
-      //   title: "Cancel",
-      //   layout: { centerX: 0, centerY: -40, width: 80, height: 21 }
-      // }),
-      // 
-      // SC.LabelView.design({
-      //   layout: { top: 20, left: 20, width: 100, height: 21 },
-      //   value: "Hello!",
-      //   valueBinding: "ViewBuilder.mainController.title"
-      // }) 
-    ]//,  
+      SC.ButtonView.design({
+        title: "OK",
+        layout: { bottom: 20, right: 20, width: 80, height: 21 }
+      }),
+      
+      SC.ButtonView.design({
+        title: "Cancel",
+        layout: { centerX: 0, centerY: -40, width: 80, height: 21 }
+      }),
+      
+      SC.LabelView.design({
+        layout: { top: 20, left: 20, width: 100, height: 21 },
+        value: "Hello!",
+        valueBinding: "ViewBuilder.mainController.title"
+      }) 
+    ],  
 
     // outlets point to particular views.  You can use these to build new 
     // views that are not in the view hierarchy, or to point to other views
     // or other objects.
-    // okButton: SC.outlet('page.mainPane.childViews.0'),
-    // cancelButton: SC.outlet('page.mainPane.childViews.1'),
-    // label: SC.outlet('page.mainPane.childViews.2')
+    okButton: SC.outlet('page.mainPane.childViews.0'),
+    cancelButton: SC.outlet('page.mainPane.childViews.1'),
+    label: SC.outlet('page.mainPane.childViews.2')
   })
   
 });
