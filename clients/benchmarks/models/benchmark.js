c86af8d9f898ab92553bea69372244777c234843
Erich Ocean <erich@atlasocean.com>
2009-02-08 00:11:05 -0800

    run benchmarks in-order

--- clients/benchmarks/models/benchmark.js
+++ clients/benchmarks/models/benchmark.js
@@ -5,16 +5,16 @@
 require('core');
 
 /** @class
-
+  
   (Document your class here)
-
+  
   @extends SC.Record
   @author AuthorName
   @version 0.1
 */
 Benchmarks.Benchmark = SC.Record.extend(
 /** @scope Benchmarks.Benchmark.prototype */ {
-
+  
   // finds all the benchmarks declared in this object and runs them.
   // benchmarks must end in the word "Benchmark"
   run: function() {
@@ -23,15 +23,14 @@ Benchmarks.Benchmark = SC.Record.extend(
     for(var key in attrs) {
       if (key.toLowerCase().match(/^benchmark/)) methods.push(key) ;
     }
-
+    
     var setup = this.get('setup') ;
     if (SC.typeOf(setup) != SC.T_FUNCTION) setup = null;
-
+    
     var teardown = this.get('teardown') ;
     if (SC.typeOf(teardown) != SC.T_FUNCTION) teardown = null;
     
-    var idx = methods.length;
-    while(--idx >= 0) {
+    for (var idx=0, len=methods.length; idx<len; ++idx) {
       var method = methods[idx] ;
       var name = method.slice(9, method.length).gsub(/\[.+x\]$/,'').trim() ;
       
@@ -42,11 +41,11 @@ Benchmarks.Benchmark = SC.Record.extend(
       
       // define new context object
       var context = {} ;
-
+      
       if (setup) setup(context) ;
       SC.Benchmark.bench(attrs[method], name, reps, context) ;
       if (teardown) teardown(context) ;
     }
   }
-
-}) ;
+  
+});
