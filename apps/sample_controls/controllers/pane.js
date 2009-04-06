d8a8bab451148b3fa3ffce351436f545b7854457
Charles Jolley <charles@sproutit.com>
2009-04-05 22:04:59 -0700

    Fix panes to use proper layout

--- apps/sample_controls/controllers/pane.js
+++ apps/sample_controls/controllers/pane.js
@@ -1,6 +1,7 @@
 // ==========================================================================
 // SampleControls.PaneController
 // ==========================================================================
+/*globals SampleControls */
 
 sc_require('core');
 
@@ -18,96 +19,133 @@ SampleControls.paneController = SC.Object.create(
   pane: null,
 
   showPanelPane: function() {
-    pane = SC.PanelPane.create({
+    var pane = SC.PanelPane.create({
+      layout: { width: 400, height: 200, centerX: 0, centerY: 0 },
       contentView: SC.View.extend({
-        layout: { width: 400, height: 200, centerX: 0, centerY: 0 }
+        layout: { top: 0, left: 0, bottom: 0, right: 0 },
+        childViews: 'labelView buttonView'.w(),
+
+        labelView: SC.LabelView.extend({
+          layout: { centerY: -10, height: 24, left: 0, right: 0 },
+          textAlign: SC.ALIGN_CENTER,
+          controlSize: SC.LARGE_CONTROL_SIZE,
+          value: "SC.PanelPane"
+        }),
+        
+        buttonView: SC.ButtonView.extend({
+          layout: { width: 80, bottom: 20, height: 21, centerX: 0 },
+          title: "Hide",
+          action: "remove",
+          target: "SampleControls.paneController.pane"
+        })
       })
     });
     pane.append();
+    this.set('pane', pane);
   },
 
   showSheetPane: function() {
-    pane = SC.SheetPane.create({
+    var pane = SC.SheetPane.create({
+      layout: { width: 400, height: 200, centerX: 0 },
       contentView: SC.View.extend({
-        layout: { width: 400, height: 200, centerX: 0 }
       })
     });
     pane.append();
   },
 
   showAlertPaneWarn: function() {
-    pane = SC.AlertPane.warn("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
+    var pane = SC.AlertPane.warn("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
     pane.append();
   },
 
   showAlertPaneError: function() {
-    pane = SC.AlertPane.error("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
+    var pane = SC.AlertPane.error("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
     pane.append();
   },
 
   showAlertPaneInfo: function() {
-    pane = SC.AlertPane.info("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
+    var pane = SC.AlertPane.info("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
     pane.append();
   },
 
   showAlertPanePlain: function() {
-    pane = SC.AlertPane.plain("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
+    var pane = SC.AlertPane.plain("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', this);      
     pane.append();
   },
 
   showAlertPaneShow: function() {
-    pane = SC.AlertPane.show("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', 'sc-icon-tools-24', this);      
+    var pane = SC.AlertPane.show("AlertPane.message", 'AlertPane.description', 'AlertPane.caption', "OK", "Cancel", 'Other...', 'sc-icon-tools-24', this);      
     pane.append();
   },
 
   showPalettePane: function() {
-    pane = SC.PalettePane.create({
+    var pane = SC.PalettePane.create({
       layout: { width: 400, height: 200, right: 20, top: 0 },
       contentView: SC.View.extend({
-        layout: { width: 400, height: 200, right: 0, top: 0 }
+        layout: { top: 0, left: 0, bottom: 0, right: 0 },
+        childViews: 'labelView buttonView'.w(),
+
+        labelView: SC.LabelView.extend({
+          layout: { centerY: -10, height: 24, left: 0, right: 0 },
+          textAlign: SC.ALIGN_CENTER,
+          controlSize: SC.LARGE_CONTROL_SIZE,
+          value: "SC.PalettePane"
+        }),
+        
+        buttonView: SC.ButtonView.extend({
+          layout: { width: 80, bottom: 20, height: 21, centerX: 0 },
+          title: "Hide",
+          action: "remove",
+          target: "SampleControls.paneController.palettePane"
+        })
       })
     });
     pane.append();
+    this.set('palettePane', pane);
   },
 
   showPickerPaneDefault: function(view) {
-    pane = SC.PickerPane.create({
+    var pane = SC.PickerPane.create({
+      layout: { width: 300, height: 200 },
       contentView: SC.View.extend({
-        layout: { width: 300, height: 200 }
+        layout: { top: 0, left: 0, bottom: 0, right: 0 }
       })
     });
     pane.popup(view);
   },
 
   showPickerPaneMenu: function(view) {
-    pane = SC.PickerPane.create({
+    var pane = SC.PickerPane.create({
+      layout: { width: 300, height: 200 },
       contentView: SC.View.extend({
-        layout: { width: 300, height: 200 }
+        layout: { top: 0, left: 0, bottom: 0, right: 0 }
       })
     });
     pane.popup(view, SC.PICKER_MENU);
   },
 
   showPickerPaneFixed: function(view) {
-    pane = SC.PickerPane.create({
+    var pane = SC.PickerPane.create({
+      layout: { width: 300, height: 200 },
       contentView: SC.View.extend({
-        layout: { width: 300, height: 200 }
+        layout: { top: 0, left: 0, bottom: 0, right: 0 }
       })
     });
     pane.popup(view, SC.PICKER_FIXED);
   },
 
   showPickerPanePointer: function(view) {
-    pane = SC.PickerPane.create({
+    var pane = SC.PickerPane.create({
+      layout: { width: 300, height: 200 },
       contentView: SC.View.extend({
-        layout: { width: 300, height: 200 }
+        layout: { top: 0, left: 0, bottom: 0, right: 0 }
       })
     });
     pane.popup(view, SC.PICKER_POINTER, [3,0,1,2,2]);
   },
 
   hidePane: function() {
-    pane.remove();
+    this.pane.remove();
   }
 
 }) ;
