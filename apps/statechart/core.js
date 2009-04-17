48b545461ea21a6470994a9c272b7a63e847e0dc
Erich Ocean <erich.ocean@me.com>
2009-04-17 14:31:28 -0700

    change sc_transitioned() implementation to use state keys, not state functions

--- apps/statechart/core.js
+++ apps/statechart/core.js
@@ -42,12 +42,12 @@ Statechart = SC.Object.create({
       case SC.EVT_INIT_SIG: {
         console.log(".. init s substates");
         // return sc_transition('s11') ;
-        return (this[this.stateKey] = this['s11'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's11', SC.EVT_TRANSITION_RES) ;
       }
       case 'e': {
         console.log("pressed 'e' in s");
         // return sc_transition('s11') ;
-        return (this[this.stateKey] = this['s11'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's11', SC.EVT_TRANSITION_RES) ;
       }
       case 'i': {
         if (this.foo) {
@@ -77,36 +77,36 @@ Statechart = SC.Object.create({
       case SC.EVT_INIT_SIG: {
         console.log(".. init s1 substates");
         // return sc_transition('s11') ;
-        return (this[this.stateKey] = this['s11'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's11', SC.EVT_TRANSITION_RES) ;
       }
       case 'a': {
         console.log("pressed 'a' in s1");
         // return sc_transition('s1') ;
-        return (this[this.stateKey] = this['s1'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's1', SC.EVT_TRANSITION_RES) ;
       }
       case 'b': {
         console.log("pressed 'b' in s1");
         // return sc_transition('s11') ;
-        return (this[this.stateKey] = this['s11'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's11', SC.EVT_TRANSITION_RES) ;
       }
       case 'c': {
         console.log("pressed 'c' in s1");
         // return sc_transition('s2') ;
-        return (this[this.stateKey] = this['s2'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's2', SC.EVT_TRANSITION_RES) ;
       }
       case 'd': {
         if (!this.foo) {
           console.log("pressed 'd' in s1 && this.foo == NO");
           this.foo = YES ;
           // return sc_transition('s') ;
-          return (this[this.stateKey] = this['s'], SC.EVT_TRANSITION_RES) ;
+          return (this[this.stateKey] = 's', SC.EVT_TRANSITION_RES) ;
         }
         break ;
       }
       case 'f': {
         console.log("pressed 'f' in s1");
         // return sc_transition('s211') ;
-        return (this[this.stateKey] = this['s211'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's211', SC.EVT_TRANSITION_RES) ;
       }
       case 'i': {
         console.log("pressed 'i' in s1");
@@ -130,19 +130,19 @@ Statechart = SC.Object.create({
           console.log("pressed 'd' in s11 && this.foo == YES");
           this.foo = NO ;
           // return sc_transition('s1') ;
-          return (this[this.stateKey] = this['s1'], SC.EVT_TRANSITION_RES) ;
+          return (this[this.stateKey] = 's1', SC.EVT_TRANSITION_RES) ;
         }
         break;
       }
       case 'g': {
         console.log("pressed 'g' in s11");
         // return sc_transition('s21') ;
-        return (this[this.stateKey] = this['s21'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's21', SC.EVT_TRANSITION_RES) ;
       }
       case 'h': {
         console.log("pressed 'h' in s11");
         // return sc_transition('s') ;
-        return (this[this.stateKey] = this['s'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's', SC.EVT_TRANSITION_RES) ;
       }
     }
   }.state('s1'),
@@ -160,17 +160,17 @@ Statechart = SC.Object.create({
       case SC.EVT_INIT_SIG: {
         console.log(".. init s2 substates");
         // return sc_transition('s211') ;
-        return (this[this.stateKey] = this['s211'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's211', SC.EVT_TRANSITION_RES) ;
       }
       case 'c': {
         console.log("pressed 'c' in s2");
         // return sc_transition('s1') ;
-        return (this[this.stateKey] = this['s1'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's1', SC.EVT_TRANSITION_RES) ;
       }
       case 'f': {
         console.log("pressed 'f' in s2");
         // return sc_transition('s11') ;
-        return (this[this.stateKey] = this['s11'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's11', SC.EVT_TRANSITION_RES) ;
       }
       case 'i': {
         if (!this.foo) {
@@ -196,22 +196,22 @@ Statechart = SC.Object.create({
       case SC.EVT_INIT_SIG: {
         console.log(".. init s21 substates");
         // return sc_transition('s211') ;
-        return (this[this.stateKey] = this['s211'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's211', SC.EVT_TRANSITION_RES) ;
       }
       case 'a': {
         console.log("pressed 'a' in s21");
         // return sc_transition('s21') ;
-        return (this[this.stateKey] = this['s21'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's21', SC.EVT_TRANSITION_RES) ;
       }
       case 'b': {
         console.log("pressed 'b' in s21");
         // return sc_transition('s211') ;
-        return (this[this.stateKey] = this['s211'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's211', SC.EVT_TRANSITION_RES) ;
       }
       case 'g': {
         console.log("pressed 'g' in s21");
         // return sc_transition('s1') ;
-        return (this[this.stateKey] = this['s1'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's1', SC.EVT_TRANSITION_RES) ;
       }
     }
   }.state('s2'),
@@ -229,12 +229,12 @@ Statechart = SC.Object.create({
       case 'd': {
         console.log("pressed 'd' in s211");
         // return sc_transition('s2') ;
-        return (this[this.stateKey] = this['s2'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's2', SC.EVT_TRANSITION_RES) ;
       }
       case 'h': {
         console.log("pressed 'h' in s211");
         // return sc_transition('s') ;
-        return (this[this.stateKey] = this['s'], SC.EVT_TRANSITION_RES) ;
+        return (this[this.stateKey] = 's', SC.EVT_TRANSITION_RES) ;
       }
     }
   }.state('s21')
