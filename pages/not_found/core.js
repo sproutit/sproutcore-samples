2b29b55e3a74fa2f26973d2a6d32f01242d3c7e0
Charles Jolley <charles@sproutit.com>
2009-07-27 03:43:53 -0700

    Add sample pages for not found

---
+++ pages/not_found/core.js
@@ -0,0 +1,22 @@
+// ==========================================================================
+// Project:   NotFound
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals NotFound */
+
+/** @namespace
+
+  Static page JavaScript
+  
+  @extends SC.Object
+*/
+NotFound = SC.Object.create(
+  /** @scope NotFound.prototype */ {
+
+  NAMESPACE: 'NotFound',
+  VERSION: '0.1.0'
+  
+  // TODO: You can place any JavaScript you need on your page here.  If you
+  // don't want any JavaScript, just delete this file.
+
+}) ;
