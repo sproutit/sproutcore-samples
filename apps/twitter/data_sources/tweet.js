b856636c9c3ab7765008999ef26bed9e58df1ce7
Juan Pinzon <juanpin@da0602a-dhcp188.apple.com>
2010-01-15 12:44:17 -0800

    Data source generated

---
+++ apps/twitter/data_sources/tweet.js
@@ -0,0 +1,64 @@
+// ==========================================================================
+// Project:   Twitter.TweetDataSource
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Twitter */
+
+/** @class
+
+  (Document Your Data Source Here)
+
+  @extends SC.DataSource
+*/
+Twitter.TweetDataSource = SC.DataSource.extend(
+/** @scope Twitter.TweetDataSource.prototype */ {
+
+  // ..........................................................
+  // QUERY SUPPORT
+  // 
+
+  fetch: function(store, query) {
+
+    // TODO: Add handlers to fetch data for specific queries.  
+    // call store.dataSourceDidFetchQuery(query) when done.
+
+    return NO ; // return YES if you handled the query
+  },
+
+  // ..........................................................
+  // RECORD SUPPORT
+  // 
+  
+  retrieveRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to retrieve an individual record's contents
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  createRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit new records to the data source.
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  updateRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit modified record to the data source
+    // call store.dataSourceDidComplete(storeKey) when done.
+
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  destroyRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to destroy records on the data source.
+    // call store.dataSourceDidDestroy(storeKey) when done
+    
+    return NO ; // return YES if you handled the storeKey
+  }
+  
+}) ;
