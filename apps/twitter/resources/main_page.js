df848c954f4724a0dfc3a3bf1fe77b2ff7f9fb36
Juan Pinzon <juanpin@da0602a-dhcp188.apple.com>
2010-01-15 11:33:56 -0800

    Added views layout and css for topbar

--- apps/twitter/resources/main_page.js
+++ apps/twitter/resources/main_page.js
@@ -11,14 +11,64 @@ Twitter.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
+    childViews: 'middleView topView'.w(),
+
+    topView: SC.View.design({
+      classNames: 'topBar'.w(),
+      layout: { top: 0, left: 0, right: 0, height: 70 },
+      childViews: 'twitterLogo searchField'.w(),
+
+      twitterLogo: SC.ImageView.design({
+        layout: { centerY: 0, height: 55, left: 20, width: 225 },
+        value:   'http://a1.twimg.com/a/1257878623/images/logo.png',
+        useImageCache:NO
+      }),
+      searchField: SC.TextFieldView.design({
+        layout: { centerY: 0, height: 25, left: 270, width: 300 },
+        hint:  "Type your search here",
+        valueBinding: 'Twitter.twettController.query'
+      })
+    }),
     
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
-      value: "Welcome to SproutCore!"
+    
+    middleView: SC.SplitView.design({
+      layout: { top: 70, bottom: 0, left: 0, right: 0 },
+      layoutDirection: SC.LAYOUT_HORIZONTAL,
+      defaultThickness: 0.5,
+      autoresizeBehavior: SC.RESIZE_BOTTOM_RIGHT,
+      canCollapseViews: YES,
+      topLeftMinThickness: 200,
+      topLeftMaxThickness: 900,
+      topLeftView: SC.ScrollView.design({
+        hasHorizontalScroller: NO,
+        layout: { top: 70, bottom: 0, left: 0, right: 0 },
+        backgroundColor: 'white',
+
+        contentView: SC.ListView.design({   
+          classNames: ['twetts'],
+          contentBinding: 'Twitter.twettController.arrangedObjects',
+          selectionBinding: 'Twitter.twettController.selection' ,
+          contentValueKey: "text",     
+          hasContentIcon: YES,
+          contentIconKey: 'profile_image_url',
+          rowHeight: 60
+        })
+      }),
+
+      dividerView: SC.SplitDividerView,
+
+      bottomRightView: SC.View.design({
+        childViews: 'address webView'.w(),
+      
+        address: SC.LabelView.design({
+          layout: { top: 0, left: 10, right: 0, height: 25 },
+          valueBinding: 'Twitter.twettController.url'
+        }),  
+        webView: SC.WebView.design({
+          layout: { top:25, left: 0, right: 0, bottom: 0 },
+          valueBinding:'Twitter.twettController.url'
+        })
+      })
     })
   })
-
 });
