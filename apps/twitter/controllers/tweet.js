04081584c0ed04e6a2b4984681fa06bca673bd08
Juan Pinzon <juanpin@da0602a-dhcp188.apple.com>
2010-01-15 12:22:35 -0800

    Bind list with controller, load fixtures

---
+++ apps/twitter/controllers/tweet.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   Twitter.tweetController
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Twitter */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.ArrayController
+*/
+Twitter.tweetController = SC.ArrayController.create(
+/** @scope Twitter.tweetController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/twitter/tests/controllers/tweet.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Twitter.tweetController Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Twitter module test ok equals same stop start */
+
+module("Twitter.tweetController");
+
+// TODO: Replace with real unit test for Twitter.tweetController
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
