d17c908125a6e139b5a64a9ad22f4dcc0e188f85
Juan Pinzon <juanpin@da0602a-dhcp188.apple.com>
2010-01-15 12:04:56 -0800

    Generated and modified model

---
+++ apps/twitter/models/tweet.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+// Project:   Twitter.Tweet
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Twitter */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Twitter.Tweet = SC.Record.extend(
+/** @scope Twitter.Tweet.prototype */ {
+
+  primaryKey: "id",
+
+  text: SC.Record.attr(String),
+  
+  profileImage: SC.Record.attr(String, { key: "profile_image_url" })
+
+}) ;
